ee5ca502f8fab6e2a87473b95d36af95531a500d
CaIeb <cdistel@498robotics.org>
2023-08-10 16:27:44 -0700

    Add Lazy Drivers
    Modified versions of CANSparkMax and TalonFX that will reduce CAN utilization

---
+++ src/main/java/org/team498/lib/drivers/LazySparkMax.java
@@ -0,0 +1,31 @@
+package org.team498.lib.drivers;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.REVLibError;
+
+public class LazySparkMax extends CANSparkMax {
+    private double currentSetpoint = Double.NaN;
+    private IdleMode currentIdleMode = null;
+    private final double epsilon = 0.01; // Minimum allowable change in speed per loop
+
+    public LazySparkMax(int deviceNumber, MotorType motorType) {
+        super(deviceNumber, motorType);
+    }
+
+    @Override
+    public void set(double setpoint) {
+        if (Math.abs(setpoint - currentSetpoint) >= epsilon) {
+            this.currentSetpoint = setpoint;
+            super.set(setpoint);
+        }
+    }
+
+    @Override
+    public REVLibError setIdleMode(IdleMode idleMode) {
+        if (currentIdleMode != idleMode) {
+            currentIdleMode = idleMode;
+            return super.setIdleMode(idleMode);
+        }
+        return REVLibError.kOk;
+    }
+}
