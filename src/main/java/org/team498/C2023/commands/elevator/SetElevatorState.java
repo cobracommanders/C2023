5216c038dbc3ad53505d1863129502c742dfd5b8
CaIeb <cdistel@498robotics.org>
2023-08-10 21:25:20 -0700

    Added SetState Command for Elevator

---
+++ src/main/java/org/team498/C2023/commands/elevator/SetElevatorState.java
@@ -0,0 +1,25 @@
+package org.team498.C2023.commands.elevator;
+
+import org.team498.C2023.State;
+import org.team498.C2023.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/*
+ *  Most subsystems will have a Wrapper class that casts the setState() method to a command
+ *  These commands typically just run the setState() method, but can be modified to do additional tasks as well
+ */
+public class SetElevatorState extends CommandBase {
+
+    private final State.Elevator state;
+
+    public SetElevatorState(State.Elevator state) {
+        this.state = state;
+    }
+
+    // The initialize() method runs once when the command starts
+    @Override
+    public void initialize() {
+        Elevator.getInstance().setState(state); // set the state of the elevator using setState()
+    }
+}
