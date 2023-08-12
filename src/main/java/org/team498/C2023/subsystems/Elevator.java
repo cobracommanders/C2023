0d435bc81467bdfcee8c47d3bda670a2629c91ab
GavinDistel <gdistel@498robotics.org>
2023-08-12 09:43:39 -0700

    Update Elevator.java

--- src/main/java/org/team498/C2023/subsystems/Elevator.java
+++ src/main/java/org/team498/C2023/subsystems/Elevator.java
@@ -3,6 +3,8 @@ package org.team498.C2023.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
+    private final LazyTalonFX fMotor;
+    private final LazyTalonFX bMotor;
     public Elevator() {
     }
 }
